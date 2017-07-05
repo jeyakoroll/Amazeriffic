edd7491d7befc89c56c99bfe92b18fc5f6632191
Jenya Korolenko <jeyakoroll@gmail.com>
2017-07-06 01:01:44 +0200

    Tasks are added by the user

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,72 @@
-const main = function() {
+const main = () => {
   "use strict";
-  $(".comment-input button").on("click", function (e){
-    // const
-    //       $new_comment = $('<p>'), // create parent node
-    //       comment_text = $(".comment-input input").val(); // save text from the input
-    //
-    // $new_comment.text(comment_text); // append text in the node
-    // $('.comments').append($new_comment); // view node in comments
+// Variables
+let toDos = [
+  'Закончить читать эту книгу',
+  'Вывести Барбоса на прогулку в парк',
+  'Ответить на почту',
+  'Подготовится к тринировке в понедельник',
+  'Обновить несколько новых задач',
+  'Купить продукты'
+],
+  toDosNew = [
+  'Сделать несколько макетов по верстке',
+  'Научится писать приложения на React',
+  'Выучить азы по NodeJS',
+  'Помочь другу с ремонтом'
+];
 
-  let $new_comment = '';
-  if($('.comment-input input').val() !== '') {
-    $new_comment = $('<p>').text($('.comment-input input').val());
-    $('.comments').append($new_comment);
-  }
+// Function
+  // Make all tabs inactive
+  // Make the first tab active
+  // Clear the main content to redefine it
+  // Returns false since we do not follow the link
 
+$('.tabs a span').toArray().forEach((elem) => {
+  // Create a click handler for this item
+  $(elem).on('click', () => {
+    let $elem = $(elem),
+        $content = $('<ul>'),
+        $input = $('<input>').addClass('input'),
+        $button = $('<button type="submit" name="addTask">').
+                  addClass('button').
+                  text('Добавить');
+
+
+    $('.tabs span').removeClass('active');
+    $elem.addClass('active');
+    $('main .content').empty();
+    // content for first tab
+      if ($elem.parent().is(":nth-child(1)")) {
+        $content;
+        toDosNew.forEach((todo) => {
+          $content.append($('<li>').text(todo));
+        });
+        $('main .content').append($content);
+        // content for second tab
+      } else if ($elem.parent().is(":nth-child(2)")) {
+        $content;
+        toDos.forEach((todo) => {
+          $content.append($('<li>').text(todo));
+        });
+        $('main .content').append($content);
+        // content for third tab
+      } else if ($elem.parent().is(":nth-child(3)")) {
+        $input, $button;
+
+        $button.on('click', () => {
+          toDosNew.push($input.val());
+          $input.val('');
+        });
+
+        $('main .content').append($input, $button);
+      }
+    return false;
   });
+});
+
+
+
+$('.tabs a:first-child span').trigger('click');
 };
 $(document).ready(main);
